aeb4d98a240c33e60c1ed0990450f3f21663f52b
Adekoye Adewale <adekoyeadewale@gmail.com>
2023-03-28 09:41:43 +0100

    responsiveness fixed

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import "./Carousel.css"
-import Star from '../assets/Stars Icon.svg'
+// import Media from '../assets/MediatekNg.svg'
 import "@splidejs/react-splide/css";
 import { 
-    Options, 
+    Options,
+    SplideTrack, 
     Splide, 
     SplideSlide 
 } from "@splidejs/react-splide";
 
 const options: Options = {
-    perPage: 2,
+    perPage: 3,
+    type: 'loop',
+    autoplay: true,
     arrows: false,
     pagination: false,
     rewind: true,
@@ -21,25 +24,25 @@ const slides = [
         name: "Mediatek Ng",
         text: "mediatekng.com",
         icon: "/static/media/Stars%20Icon.233149f8ef135a0d51813dfdf11778a0.svg",
-        image: "/static/media/JavaScript.3bb36fd89f0277e27fd5fa1e744820c7.svg",
+        image: "https://adewaleimg.netlify.app/img-assests/MediatekNg.webp",
     },
     {
         name: "Reset USA",
         text: "reset-usa.com",
         icon: "/static/media/Stars%20Icon.233149f8ef135a0d51813dfdf11778a0.svg",
-        image: "/static/media/JavaScript.3bb36fd89f0277e27fd5fa1e744820c7.svg",
+        image: "https://adewaleimg.netlify.app/img-assests/Reset-USA%20Mockup.webp",
     },
     {
         name: "Custom QR Code Generator",
         text: "custom-qrcode.netlify.app",
         icon: "/static/media/Stars%20Icon.233149f8ef135a0d51813dfdf11778a0.svg",
-        image: "/static/media/JavaScript.3bb36fd89f0277e27fd5fa1e744820c7.svg",
+        image: "https://adewaleimg.netlify.app/img-assests/Custom-QR-CODE.webp",
     },
     {
         name: "Ask Shola",
         text: "askshola.com",
         icon: "/static/media/Stars%20Icon.233149f8ef135a0d51813dfdf11778a0.svg",
-        image: "/static/media/JavaScript.3bb36fd89f0277e27fd5fa1e744820c7.svg",
+        image: "https://adewaleimg.netlify.app/img-assests/ASK%20SHOLA.webp",
     },
 ];
 
@@ -48,7 +51,10 @@ export default function Carousel() {
     <>
         <div className='wrapper'>
             <div className='splide'>
-                <Splide options={options}>
+                <Splide options={options}
+                        aria-labelledby="autoplay-example-heading"
+                        hasTrack={ false }>
+                            <SplideTrack>
                     {slides.map((slide) => (
                         <SplideSlide>
                             <div className='slide'>
@@ -59,16 +65,17 @@ export default function Carousel() {
                                                 <h3>{slide.name}</h3>
                                                 <p>{slide.text}</p>
                                             </div>
-                                            <img src={slide.icon} alt='Open Link' />
+                                            <img src={slide.icon} alt='stars' />
                                         </a>
                                     </div>
                                     <div className='carousel-img'>
-                                        <img src={slide.image} alt='Open Link' />
+                                        <img src={slide.image} alt={slide.name} />
                                     </div>
                                 </div>
                             </div>
-                        </SplideSlide>
+                        </SplideSlide>        
                     ))}
+                    </SplideTrack>
                 </Splide>
             </div>
         </div>
--- src/components/ProjectLists.js
+++ src/components/ProjectLists.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import './ProjectLists.css'
 import ProjectItem from './CardItem'
-import ResetUSA from '../assets/ResetUSA.svg'
 import OpenLink from '../assets/Open-Link-Icon.svg';
 
 const projectsFile = [
@@ -10,56 +9,56 @@ const projectsFile = [
             icon: "",
             url: "https://www.reset-usa.com",
             urlname: "reset-usa.com",
-            image: "",
+            image: "https://adewaleimg.netlify.app/img-assests/Reset-USA%20Mockup.webp",
             imagename: "Reset USA",
         }, {
             title: "MediatekNg",
             icon: "",
             url: "https://www.mediatekng.com/",
             urlname: "mediatekng.com",
-            image: "../assets/github.svg",
+            image: "https://adewaleimg.netlify.app/img-assests/MediatekNg.webp",
             imagename: "Mediatek Ng",
         }, {
             title: "Custom QR Code Generator",
             icon: "",
             url: "https://custom-qrcode.netlify.app/",
             urlname: "adekoyeadewale.com",
-            image: "",
+            image: "https://adewaleimg.netlify.app/img-assests/Custom-QR-CODE.webp",
             imagename: "Custom QR Code",
         }, {
             title: "Responsive-MegaMenu",
             icon: "",
             url: "",
             urlname: "adekoyeadewale.com",
-            image: "",
+            image: "https://adewaleimg.netlify.app/img-assests/ASK%20SHOLA.webp",
             imagename: "Responsive-MegaMenu",
         }, {
             title: "AskShola",
             icon: "",
             url: "https://askshola.com/",
             urlname: "askshola.com",
-            image: "",
+            image: "https://adewaleimg.netlify.app/img-assests/ASK%20SHOLA.webp",
             imagename: "Askshola",
         }, {
             title: "ScrapBeta",
             icon: "",
             url: "https://scrapbeta.com/",
             urlname: "scrapbeta.com",
-            image: "",
+            image: "https://adewaleimg.netlify.app/img-assests/ASK%20SHOLA.webp",
             imagename: "Scrapbeta",
         }, {
             title: "FinnGrey",
             icon: "",
             url: "https://finngreyprojects.com/",
             urlname: "finngreyprojects.com",
-            image: "",
+            image: "https://adewaleimg.netlify.app/img-assests/ASK%20SHOLA.webp",
             imagename: "Finngreyprojects",
         }, {
             title: "Ayoola Photography",
             icon: "",
             url: "https://ayoolaoluwatosin.com/",
             urlname: "ayoolaoluwatosin.com",
-            image: "",
+            image: "https://adewaleimg.netlify.app/img-assests/ASK%20SHOLA.webp",
             imagename: "Ayoolaoluwatosin",
         }
     ];
@@ -74,7 +73,7 @@ export default function ProjectLists() {
                         title={projectFile.title} 
                         link={projectFile.url} 
                         linkName={projectFile.urlname}
-                        image={ResetUSA}
+                        image={projectFile.image}
                         imageName={projectFile.imagename}/>
             )
             )}
