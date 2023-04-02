bbfae1d0042e529ccca9d47dd10675aada639473
Adekoye Adewale <adekoyeadewale@gmail.com>
2023-04-02 17:38:21 +0100

    error 404 page bug fixed
    The error 404 page bug fixed was fixed by adding some lines tothe package.json file and also install the Express server

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Serve static files from the build directory
+app.use(express.static(path.join(__dirname, 'build')));
+
+// Always serve the index.html file for any request
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+// Start the server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server started on http://localhost:${port}`);
+});
