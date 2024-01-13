d8a052cd43f27844f7d27f6726c762f50365ff8d
Adekoye Adewale <adekoyeadewale@gmail.com>
2024-01-14 00:18:36 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
-const express = require('express');
-const path = require('path');
-const app = express();
+// const express = require('express');
+// const path = require('path');
+// const app = express();
 
-// Serve static files from the build directory
-app.use(express.static(path.join(__dirname, 'build')));
+// // Serve static files from the build directory
+// app.use(express.static(path.join(__dirname, 'build')));
 
-// Always serve the index.html file for any request
-app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname, './public/index.html'), function(err) {
-      if (err) {
-        res.status(500).send(err)
-      }
-    })
-  })
+// // Always serve the index.html file for any request
+// app.get('/*', function(req, res) {
+//     res.sendFile(path.join(__dirname, './public/index.html'), function(err) {
+//       if (err) {
+//         res.status(500).send(err)
+//       }
+//     })
+//   })
   
-// Start the server
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Server started on http://localhost:${port}`);
-});
+// // Start the server
+// const port = process.env.PORT || 3000;
+// app.listen(port, () => {
+//   console.log(`Server started on http://localhost:${port}`);
+// });
